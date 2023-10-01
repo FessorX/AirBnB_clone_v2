469f11041ed5824465a02c149b262de2298d4c15
Musa Abraham <profaimusa@gmail.com>
2023-10-01 09:38:17 -0700

    more

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ["34.234.203.232", "54.85.64.225"]
+env.hosts = ["34.73.0.174", "35.196.78.105"]
 """The list of host server IP addresses."""
 
 
